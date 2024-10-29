3b99a910eb0c8fe040c21d2a917be1bfcd035a6b
Gerd Braun <webmaster@your-d-sign.de>
2024-10-29 11:39:50 +0100

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,30 @@
-const Navbar = () => {
-  return <nav>Navbar</nav>;
-};
+import { NavLink } from "react-router-dom";
 
-export default Navbar;
+const Navbar = () => {
+    return (
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <NavLink className="btn btn-ghost text-xl" to="/">Event Scheduler</NavLink>
+          </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink to="/">home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/events/add">add event (protected)</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup">sign up (hidden when logged in)</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Navbar;
+  
