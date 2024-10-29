54ee90d0444213fcdb4e007a9311d3f4180f8896
Gerd Braun <webmaster@your-d-sign.de>
2024-10-29 10:34:10 +0100

    splitting up in parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { Outlet } from "react-router-dom"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./components/Layout";
+import Hero from "./components/Hero";
+import EventList from "./components/EventList";
+import EventSingle from "./components/EventSingle";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={
+            <>
+              <Hero />
+              <EventList />
+            </>
+          }
+        />
+        <Route path="/events" element={<EventList />} />
+        <Route path="/events/:id" element={<EventSingle />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Route>
+    )
+  );
   return (
     <>
-   <header>
-    <nav></nav>
-   </header>
-   <main>
-    <Outlet />
-   </main>
-   <footer></footer>
-   </>
-  )
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
