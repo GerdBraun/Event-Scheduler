77d816569ec1646973873dccbc6fb855c060450a
Gerd Braun <webmaster@your-d-sign.de>
2024-10-29 15:51:10 +0100

    enabled login/logout & pass token to EventEditForm for saving

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,19 +1,47 @@
 import { useNavigate } from "react-router-dom";
 
-const LoginForm = ({rememberToken}) => {
+const LoginForm = ({ rememberToken }) => {
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('handleSubmit called');
+    console.log("handleSubmit called");
 
     // TODO: really fetch the data on submit
+    fetch("http://localhost:3001/api/auth/login", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      // body: '{\n  "email": "user@example.com",\n  "password": "password123"\n}',
+      body: JSON.stringify({
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+// TODO: error handling
+if(data.error) {
+  alert(data.error);
+  return
+}
+
+        rememberToken(data.token);
+        navigate("/");
+        //setEvent(data)
+      })
+      .catch((error) => console.error("Error fetching event details:", error));
 
     /* just for testing */
     // TODO: remove this for production
-    rememberToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJ1c2VyQGV4YW1wbGUuY29tIiwiaWF0IjoxNzMwMTk5NTIxLCJleHAiOjE3MzM3OTk1MjF9.feS_3p40GSX0l4Un3_YpWtnyCYni0hyDBz2cZhOUz0M");
-    navigate('/');
-  }
+    // rememberToken(
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJ1c2VyQGV4YW1wbGUuY29tIiwiaWF0IjoxNzMwMTk5NTIxLCJleHAiOjE3MzM3OTk1MjF9.feS_3p40GSX0l4Un3_YpWtnyCYni0hyDBz2cZhOUz0M"
+    // );
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -26,29 +54,29 @@ const LoginForm = ({rememberToken}) => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body" onSubmit={(e)=>handleSubmit(e)}>
+          <form className="card-body" onSubmit={(e) => handleSubmit(e)}>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
+                name="email"
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
-                defaultValue="user@example.com"
-                />
+              />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
               <input
+                name="password"
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
-                defaultValue="password123"
               />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
