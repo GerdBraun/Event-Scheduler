9c559e22006c0cda6ca1002368be00c1a8bc6f17
Gerd Braun <webmaster@your-d-sign.de>
2024-10-29 13:31:58 +0100

    added login / signup forms

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,19 @@
-const LoginForm = () => {
+import { useNavigate } from "react-router-dom";
+
+const LoginForm = ({rememberToken}) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('handleSubmit called');
+
+    // TODO: really fetch the data on submit
+
+    /* just for testing */
+    // TODO: remove this for production
+    rememberToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJ1c2VyQGV4YW1wbGUuY29tIiwiaWF0IjoxNzMwMTk5NTIxLCJleHAiOjE3MzM3OTk1MjF9.feS_3p40GSX0l4Un3_YpWtnyCYni0hyDBz2cZhOUz0M");
+    navigate('/');
+  }
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,7 +26,7 @@ const LoginForm = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form className="card-body" onSubmit={(e)=>handleSubmit(e)}>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -21,7 +36,7 @@ const LoginForm = () => {
                 placeholder="email"
                 className="input input-bordered"
                 required
-                value="user@example.com"
+                defaultValue="user@example.com"
                 />
             </div>
             <div className="form-control">
@@ -33,7 +48,7 @@ const LoginForm = () => {
                 placeholder="password"
                 className="input input-bordered"
                 required
-                value="password123"
+                defaultValue="password123"
               />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
