183f1c6218b88776d93c1a9ca64e2049f5512205
Shabnam <zahra.reihani85@gmail.com>
2024-11-03 11:53:32 +0100

    css change for sign in and sign up form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -11,7 +11,7 @@ const LoginForm = ({ rememberToken }) => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormState(prev => ({ ...prev, [name]: value }));
+    setFormState((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = (e) => {
@@ -23,7 +23,6 @@ const LoginForm = ({ rememberToken }) => {
         accept: "application/json",
         "Content-Type": "application/json",
       },
-      // body: '{\n  "email": "user@example.com",\n  "password": "password123"\n}',
       body: JSON.stringify({
         email: formState.email,
         password: formState.password,
@@ -31,7 +30,6 @@ const LoginForm = ({ rememberToken }) => {
     })
       .then((response) => response.json())
       .then((data) => {
-
         if (data.error) {
           alert(data.error);
           return;
@@ -42,55 +40,54 @@ const LoginForm = ({ rememberToken }) => {
       })
       .catch((error) => console.error("Error fetching event details:", error));
   };
+
+  const handleSignUpClick = () => {
+    navigate("/signup"); // Assuming the route to SignUpForm is '/signup'
+  };
+
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-          Log in nunc et incipias addendo vel emendando eventus!
-          </p>
-        </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body" onSubmit={(e) => handleSubmit(e)}>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                name="email"
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-                onChange={handleChange}
-                value={formState.email}
-                />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                name="password"
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-                onChange={handleChange}
-                value={formState.password}
-                />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </form>
-        </div>
+    <div className="hero bg-gray-200 min-h-screen flex items-center justify-center">
+      <div className="card bg-gray-700 w-full max-w-md md:max-w-lg lg:max-w-2xl p-8 shadow-2xl rounded-lg">
+        <form className="card-body" onSubmit={handleSubmit}>
+          <h2 className="text-2xl text-blue-500 font-bold mb-6 text-center">Log In</h2>
+          <div className="form-control mb-4">
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              className="input input-bordered"
+              required
+              onChange={handleChange}
+              value={formState.email}
+            />
+          </div>
+          <div className="form-control mb-4">
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              className="input input-bordered"
+              required
+              onChange={handleChange}
+              value={formState.password}
+            />
+            <label className=" text-white">
+              <a href="#" className="label-text-alt link link-hover text-blue-500 text-sm">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-4">
+            <button className="btn btn-primary text-xl mb-4">Log In</button>
+          </div>
+          <div className="flex text-center">
+            <p className=" text-white">Don't have an account?
+            <a href="#" onClick={handleSignUpClick} className="label-text-alt p-2 link link-hover text-blue-500 text-xl">
+                Sign Up
+              </a>
+              </p>
+          </div>
+        </form>
       </div>
     </div>
   );
